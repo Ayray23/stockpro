69b93865e8bc9e2c477a5b801d65ceb8aa53e153
Ayray23 <rachealadebisi23@gmail.com>
2025-07-31 07:11:39 -0700

    Added a heading to the dashboard

--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -6,6 +6,9 @@ import TransactionsTable from "../pages/transactionsTable";
 const DashboardPage = () => {
   return (
     <div className="space-y-8 pt-16 md:ml-64 p-4">
+      <h1 className="text-2xl text-center">
+        Dashboard
+      </h1>
       <SummaryCards />
       <StockTable />
       <div className="-ml-67 -mt-20">
